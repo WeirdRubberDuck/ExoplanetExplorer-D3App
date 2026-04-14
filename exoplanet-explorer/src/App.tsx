fdf3bc8e97d02b3e82b5978e947c7d4c8a2d24e7
Emma Broman <emma.broman@liu.se>
2026-04-14 17:20:22 +0200

    Set up an appshell and a colorscheme toggle

--- exoplanet-explorer/src/App.tsx
+++ exoplanet-explorer/src/App.tsx
@@ -1,7 +1,38 @@
 import "@mantine/core/styles.css";
 
-import { MantineProvider } from "@mantine/core";
+import {
+  AppShell,
+  Center,
+  createTheme,
+  Flex,
+  MantineProvider,
+} from "@mantine/core";
+import { ColorSchemeToggle } from "./components/ColorSchemeToggle";
+
+const theme = createTheme({
+  /** Your theme override here */
+});
 
 export default function App() {
-  return <MantineProvider>ExoplanetExplorer</MantineProvider>;
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <AppShell
+        padding="md"
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+        }}
+      >
+        <AppShell.Header p={"sm"}>
+          <Flex justify={"space-between"} align={"center"} h={"100%"}>
+            <Center>Exoplanet Explorer</Center>
+            <ColorSchemeToggle />
+          </Flex>
+        </AppShell.Header>
+        <AppShell.Navbar p={"sm"}>Some settings here</AppShell.Navbar>
+        <AppShell.Main>Welcome to the Exoplanet Explorer!</AppShell.Main>
+      </AppShell>
+    </MantineProvider>
+  );
 }
---
+++ exoplanet-explorer/src/components/ColorSchemeToggle.tsx
@@ -0,0 +1,35 @@
+import {
+  ActionIcon,
+  useMantineColorScheme,
+  useComputedColorScheme,
+  Tooltip,
+} from "@mantine/core";
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
+
+export function ColorSchemeToggle() {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+
+  return (
+    <Tooltip
+      label={`${computedColorScheme === "light" ? "Dark" : "Light"} mode`}
+    >
+      <ActionIcon
+        onClick={() =>
+          setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+        }
+        variant="default"
+        size="lg"
+        aria-label="Toggle color scheme"
+      >
+        {computedColorScheme === "dark" ? (
+          <MdOutlineLightMode />
+        ) : (
+          <MdOutlineDarkMode />
+        )}
+      </ActionIcon>
+    </Tooltip>
+  );
+}

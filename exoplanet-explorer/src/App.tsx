780014a476d1310092310e2722bad2cb94209c41
Emma Broman <emma.broman@liu.se>
2026-04-14 18:08:14 +0200

    Some more setup

--- exoplanet-explorer/src/App.tsx
+++ exoplanet-explorer/src/App.tsx
@@ -7,7 +7,8 @@ import {
   Flex,
   MantineProvider,
 } from "@mantine/core";
-import { ColorSchemeToggle } from "./components/ColorSchemeToggle";
+import { HomePage } from "@/pages/HomePage";
+import { ColorSchemeToggle } from "@/components/ColorSchemeToggle";
 
 const theme = createTheme({
   /** Your theme override here */
@@ -16,22 +17,16 @@ const theme = createTheme({
 export default function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme="dark">
-      <AppShell
-        padding="md"
-        header={{ height: 60 }}
-        navbar={{
-          width: 300,
-          breakpoint: "sm",
-        }}
-      >
+      <AppShell padding="md" header={{ height: 60 }}>
         <AppShell.Header p={"sm"}>
           <Flex justify={"space-between"} align={"center"} h={"100%"}>
             <Center>Exoplanet Explorer</Center>
             <ColorSchemeToggle />
           </Flex>
         </AppShell.Header>
-        <AppShell.Navbar p={"sm"}>Some settings here</AppShell.Navbar>
-        <AppShell.Main>Welcome to the Exoplanet Explorer!</AppShell.Main>
+        <AppShell.Main>
+          <HomePage />
+        </AppShell.Main>
       </AppShell>
     </MantineProvider>
   );
---
+++ exoplanet-explorer/src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+import "@mantine/core/styles.css";
+
+export function HomePage() {
+  return <>Welcome to the Exoplanet Explorer!</>;
+}

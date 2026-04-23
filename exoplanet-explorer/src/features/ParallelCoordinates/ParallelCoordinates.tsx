faf95d006395bbc7de07139a9881d50ead748324
Emma Broman <emma.broman@liu.se>
2026-04-23 11:34:54 +0200

    Resize!!

--- exoplanet-explorer/src/features/ParallelCoordinates/ParallelCoordinates.tsx
+++ exoplanet-explorer/src/features/ParallelCoordinates/ParallelCoordinates.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useRef } from "react";
-import { useViewportSize } from "@mantine/hooks";
+import { Box } from "@mantine/core";
+import { useResizeObserver, useViewportSize } from "@mantine/hooks";
 
 import { makeInteractiveDummyChart } from "./dummychart";
 import { makeParallelCoordinatesChart } from "./parallelcoordinateschart";
 
 export function ParallelCoordinates() {
+  const [containerRef, container] = useResizeObserver();
+
   const svgRef = useRef<SVGSVGElement | null>(null);
   const svgDummyRef = useRef<SVGSVGElement | null>(null);
 
-  const { width } = useViewportSize();
+  const { width, height } = useViewportSize();
 
   useEffect(() => {
     if (svgDummyRef.current) {
@@ -22,20 +25,30 @@ export function ParallelCoordinates() {
     if (svgRef.current) {
       makeParallelCoordinatesChart({
         svgRef,
-        width,
-        height: 800,
+        width: container ? container.width : 400,
+        height: container ? container.height : 400,
       });
     }
-  }, [width]);
+  }, [container, width]);
 
   return (
     <>
-      <svg
-        ref={svgRef}
-        role={"img"}
-        aria-label={"Interactive parallel coordinates plot"}
-      />
-
+      <Box
+        style={{
+          resize: "vertical",
+          height: 800,
+          maxHeight: height,
+          minHeight: 200,
+          overflow: "hidden",
+        }}
+        ref={containerRef}
+      >
+        <svg
+          ref={svgRef}
+          role={"img"}
+          aria-label={"Interactive parallel coordinates plot"}
+        />
+      </Box>
       <svg
         ref={svgDummyRef}
         role={"img"}

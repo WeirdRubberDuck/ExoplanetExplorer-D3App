a4e004663d36e5e03107a0192227bd1bb9502d97
Emma Broman <emma.broman@liu.se>
2026-07-15 16:19:35 +0200

    Load data as a json file instead

--- exoplanet-explorer/src/pages/HomePage.tsx
+++ exoplanet-explorer/src/pages/HomePage.tsx
@@ -4,15 +4,23 @@ import { Box, Group } from '@mantine/core';
 import { ParallelCoordinates } from '@/features/ParallelCoordinates/ParallelCoordinates';
 import { SelectionList } from '@/features/SelectionList/SelectionList';
 import { SettingsBar } from '@/features/SettingsBar/SettingsBar';
-import { data } from '@/public/dummydata'; // TODO: Replace with actual data
 import { initializeData } from '@/redux/data/dataSlice';
 import { useAppDispatch } from '@/redux/hooks';
+
+// TODO: Load real data from a server or local file
+const dataUrl = new URL('../data/aggregated_data.json', import.meta.url).href;
 
 export function HomePage() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(initializeData(data));
+    const loadData = async () => {
+      const response = await fetch(dataUrl);
+      const json = await response.json();
+      dispatch(initializeData(json));
+    };
+
+    void loadData();
   }, [dispatch]);
 
   return (
